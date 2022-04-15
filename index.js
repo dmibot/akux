1b5f803f171f942011de002220d30e96ac92f69d
kerreeehoreee <99607873+dmibot@users.noreply.github.com>
2022-04-16 04:53:29 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let path = require('path')
 let fs = require('fs')
 const CFonts = require('cfonts')
 
-CFonts.say('Biasalah...', {
+CFonts.say('Asiaapp...', {
     font: 'chrome',
     align: 'center',
     gradient: ['red', 'magenta']
